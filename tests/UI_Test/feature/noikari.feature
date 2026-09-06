cbbe92a8b82b36e66d761cadb9d69cfe4e7ea031
mobileautomationuser42 <mobileautomationuser42@gmail.com>
2026-09-06 18:07:07 +0530

    Bhagvnata: Addde TC02 related to apply JOB

--- tests/UI_Test/feature/noikari.feature
+++ tests/UI_Test/feature/noikari.feature
@@ -6,3 +6,11 @@ Feature: Naukri Login
     Given I navigate to the Naukri homepage
     When I log in to Naukri with valid credentials
     Then I should be logged in to Naukri successfully
+
+@TC-NAUKRI-02
+Scenario: Login and Apply for Playwright Jobs
+    Given I navigate to the Naukri homepage
+    When I log in to Naukri with valid credentials
+    Then I should be logged in to Naukri successfully
+    When I search for "Playwright Automation Tester" jobs in "Pune"
+    Then I apply to top 20 easy apply jobs
--- tests/UI_Test/fixture/fixtures.ts
+++ tests/UI_Test/fixture/fixtures.ts
@@ -1,15 +1,17 @@
 import { test as base } from 'playwright-bdd';
 import { NaukriLoginPage } from '../page/NaukriLoginPage';
-
+import { NaukriJobPage } from '../page/NaukriJobPage';
 import type { World } from '../type/world';
 
 
 type Fixtures = {
   naukriLoginPage: NaukriLoginPage;
+  naukriJobPage: NaukriJobPage;
   world: World;
 };
 
 export const test = base.extend<Fixtures>({
   naukriLoginPage: async ({ page }, use) => await use(new NaukriLoginPage(page)),
+  naukriJobPage: async ({ page }, use) => await use(new NaukriJobPage(page)),
   world: async ({}, use) => await use({} as World),
 });
